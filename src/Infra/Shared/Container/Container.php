a2103ac1a3c346b2901a04d1272e95cf01a0cd70
KerianMM <kerianmontes@gmail.com>
2023-12-12 18:10:47 +0100

    chore: create Container::boot method

--- src/Infra/Shared/Container/Container.php
+++ src/Infra/Shared/Container/Container.php
@@ -2,13 +2,43 @@
 
 namespace Kerianmm\Fleet\Infra\Shared\Container;
 
+use Kerianmm\Fleet\App\Command\CreateFleetCommand;
+use Kerianmm\Fleet\App\Command\CreateFleetCommandHandler;
+use Kerianmm\Fleet\App\Command\ParkCommand;
+use Kerianmm\Fleet\App\Command\ParkCommandHandler;
+use Kerianmm\Fleet\App\Command\RegisterVehicleCommand;
+use Kerianmm\Fleet\App\Command\RegisterVehicleCommandHandler;
+use Kerianmm\Fleet\App\Shared\Command\CommandBusInterface;
 use Kerianmm\Fleet\App\Shared\Container\ContainerInterface;
+use Kerianmm\Fleet\App\Shared\Query\QueryBusInterface;
+use Kerianmm\Fleet\Domain\Repository\FleetRepositoryInterface;
+use Kerianmm\Fleet\Domain\Repository\VehicleRepositoryInterface;
+use Kerianmm\Fleet\Infra\Repository\FleetRepository;
+use Kerianmm\Fleet\Infra\Repository\VehicleRepository;
+use Kerianmm\Fleet\Infra\Shared\CommandBus;
+use Kerianmm\Fleet\Infra\Shared\QueryBus;
 
 final class Container implements ContainerInterface
 {
     public function __construct(
         private array $services,
     ) {
+    }
+
+    public static function boot(): static
+    {
+        $container = new self([
+            FleetRepositoryInterface::class   => new FleetRepository(),
+            VehicleRepositoryInterface::class => new VehicleRepository(),
+        ]);
+
+        $container->set(CommandBusInterface::class, new CommandBus($container));
+        $container->set(QueryBusInterface::class, new QueryBus($container));
+        $container->set(CreateFleetCommand::class, new CreateFleetCommandHandler($container));
+        $container->set(RegisterVehicleCommand::class, new RegisterVehicleCommandHandler($container));
+        $container->set(ParkCommand::class, new ParkCommandHandler($container));
+
+        return $container;
     }
 
     public function set(string $id, object $service): static
